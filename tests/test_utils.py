3915497977af9a5a1501ed46cb316f7403583074
James Douglass <jdouglass@stanford.edu>
2017-06-21 11:55:22 -0700

    Refactoring the wait_on_signal context manager.
    Errors encountered are now raised instead of just logged, and some extra
    code is removed.  RE:#3610

---
+++ tests/test_utils.py
@@ -0,0 +1,279 @@
+"""Module for testing the natcap.invest.utils module.."""
+import unittest
+import os
+import tempfile
+import shutil
+import logging
+import threading
+
+from pygeoprocessing.testing import scm
+import pygeoprocessing.testing
+
+
+class SuffixUtilsTests(unittest.TestCase):
+    """Tests for natcap.invest.utils.make_suffix_string."""
+
+    def test_suffix_string(self):
+        """Utils: test suffix_string."""
+        from natcap.invest import utils
+
+        args = {'foo': 'bar', 'file_suffix': 'suff'}
+        suffix = utils.make_suffix_string(args, 'file_suffix')
+        self.assertEqual(suffix, '_suff')
+
+    def test_suffix_string_underscore(self):
+        """Utils: test suffix_string underscore."""
+        from natcap.invest import utils
+
+        args = {'foo': 'bar', 'file_suffix': '_suff'}
+        suffix = utils.make_suffix_string(args, 'file_suffix')
+        self.assertEqual(suffix, '_suff')
+
+    def test_suffix_string_empty(self):
+        """Utils: test empty suffix_string."""
+        from natcap.invest import utils
+
+        args = {'foo': 'bar', 'file_suffix': ''}
+        suffix = utils.make_suffix_string(args, 'file_suffix')
+        self.assertEqual(suffix, '')
+
+    def test_suffix_string_no_entry(self):
+        """Utils: test no suffix entry in args."""
+        from natcap.invest import utils
+
+        args = {'foo': 'bar'}
+        suffix = utils.make_suffix_string(args, 'file_suffix')
+        self.assertEqual(suffix, '')
+
+
+class FileRegistryUtilsTests(unittest.TestCase):
+    """Tests for natcap.invest.utils.file_registry."""
+
+    def test_build_file_registry(self):
+        """Utils: test build_file_registry on simple case."""
+        from natcap.invest import utils
+
+        base_dict = {'foo': 'bar', 'baz': '/bart/bam.txt'}
+        file_registry = utils.build_file_registry([(base_dict, '')], '')
+
+        self.assertEqual(
+            FileRegistryUtilsTests._norm_dict(base_dict),
+            FileRegistryUtilsTests._norm_dict(file_registry))
+
+    def test_build_file_registry_suffix(self):
+        """Utils: test build_file_registry on suffix."""
+        from natcap.invest import utils
+
+        base_dict = {'foo': 'bar', 'baz': '/bart/bam.txt'}
+        file_registry = utils.build_file_registry([
+            (base_dict, '')], '_suff')
+        expected_dict = {
+            'foo': 'bar_suff',
+            'baz': '/bart/bam_suff.txt'
+        }
+
+        self.assertEqual(
+            FileRegistryUtilsTests._norm_dict(expected_dict),
+            FileRegistryUtilsTests._norm_dict(file_registry))
+
+    def test_build_file_registry_list_suffix(self):
+        """Utils: test build_file_registry on list of files w/ suffix."""
+        from natcap.invest import utils
+
+        base_dict = {
+            'foo': ['bar', '/bart/bam.txt']
+        }
+        file_registry = utils.build_file_registry([
+            (base_dict, '')], '_suff')
+        expected_dict = {
+            'foo': ['bar_suff', '/bart/bam_suff.txt']
+        }
+
+        self.assertEqual(
+            FileRegistryUtilsTests._norm_dict(expected_dict),
+            FileRegistryUtilsTests._norm_dict(file_registry))
+
+    def test_build_file_registry_path(self):
+        """Utils: test build_file_registry on path."""
+        from natcap.invest import utils
+
+        base_dict = {
+            'foo': 'bar',
+            'baz': '/bart/bam.txt',
+            'jab': 'jim'
+        }
+        file_registry = utils.build_file_registry([
+            (base_dict, 'newpath')], '')
+        expected_dict = {
+            'foo': 'newpath/bar',
+            'jab': 'newpath/jim',
+            'baz': '/bart/bam.txt',
+        }
+
+        self.assertEqual(
+            FileRegistryUtilsTests._norm_dict(expected_dict),
+            FileRegistryUtilsTests._norm_dict(file_registry))
+
+    def test_build_file_registry_duppath(self):
+        """Utils: test build_file_registry ValueError on duplicate paths."""
+        from natcap.invest import utils
+
+        base_dict = {
+            'foo': 'bar',
+            'jab': 'bar'
+        }
+        with self.assertRaises(ValueError):
+            _ = utils.build_file_registry([
+                (base_dict, 'newpath')], '')
+
+    def test_build_file_registry_dupkeys(self):
+        """Utils: test build_file_registry ValueError on duplicate keys."""
+        from natcap.invest import utils
+
+        base_dict1 = {
+            'foo': 'bar',
+        }
+        base_dict2 = {
+            'foo': 'bar2',
+        }
+        with self.assertRaises(ValueError):
+            _ = utils.build_file_registry([
+                (base_dict1, ''), (base_dict2, '')], '')
+
+    def test_build_file_registry_invalid_value(self):
+        """Utils: test build_file_registry with invalid path type."""
+        from natcap.invest import utils
+
+        base_dict = {
+            'foo': 'bar',
+            'baz': None
+        }
+        with self.assertRaises(ValueError):
+            _ = utils.build_file_registry([(base_dict, 'somepath')], '')
+
+    @staticmethod
+    def _norm_dict(path_dict):
+        """Take a dictionary of paths and normalize the paths."""
+        result_dict = {}
+        for key, path in path_dict.iteritems():
+            if isinstance(path, str):
+                result_dict[key] = os.path.normpath(path)
+            elif isinstance(path, list):
+                result_dict[key] = [
+                    os.path.normpath(list_path) for list_path in path]
+            else:
+                raise ValueError("Unexpected path value: %s", path)
+        return result_dict
+
+
+class ExponentialDecayUtilsTests(unittest.TestCase):
+    """Tests for natcap.invest.utils.exponential_decay_kernel_raster."""
+
+    _REGRESSION_PATH = os.path.join(
+        os.path.dirname(__file__), '..', 'data', 'invest-test-data',
+        'exp_decay_kernel')
+
+    def setUp(self):
+        """Setup workspace."""
+        self.workspace_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        """Delete workspace."""
+        shutil.rmtree(self.workspace_dir)
+
+    @scm.skip_if_data_missing(_REGRESSION_PATH)
+    def test_exp_decay_kernel_raster(self):
+        """Utils: test exponential_decay_kernel_raster."""
+        from natcap.invest import utils
+        expected_distance = 100  # 10 pixels
+        kernel_filepath = os.path.join(self.workspace_dir, 'kernel_100.tif')
+        utils.exponential_decay_kernel_raster(
+            expected_distance, kernel_filepath)
+        shutil.copyfile(kernel_filepath, 'kernel.tif')
+
+        pygeoprocessing.testing.assert_rasters_equal(
+            os.path.join(
+                ExponentialDecayUtilsTests._REGRESSION_PATH,
+                'kernel_100.tif'), kernel_filepath, abs_tol=1e-6)
+
+
+class SandboxTempdirTests(unittest.TestCase):
+    def setUp(self):
+        """Setup workspace."""
+        self.workspace_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        """Delete workspace."""
+        shutil.rmtree(self.workspace_dir)
+
+    def test_sandbox_manager(self):
+        from natcap.invest import utils
+
+        with utils.sandbox_tempdir(suffix='foo',
+                                   prefix='bar',
+                                   dir=self.workspace_dir) as new_dir:
+            self.assertTrue(new_dir.startswith(self.workspace_dir))
+            basename = os.path.basename(new_dir)
+            self.assertTrue(basename.startswith('bar'))
+            self.assertTrue(basename.endswith('foo'))
+
+            # trigger the exception handling for coverage.
+            shutil.rmtree(new_dir)
+
+
+class TimeFormattingTests(unittest.TestCase):
+    def test_format_time_hours(self):
+        from natcap.invest.utils import _format_time
+
+        seconds = 3667
+        self.assertEqual(_format_time(seconds), '1h 1m 7s')
+
+    def test_format_time_minutes(self):
+        from natcap.invest.utils import _format_time
+
+        seconds = 67
+        self.assertEqual(_format_time(seconds), '1m 7s')
+
+    def test_format_time_seconds(self):
+        from natcap.invest.utils import _format_time
+
+        seconds = 7
+        self.assertEqual(_format_time(seconds), '7s')
+
+
+class ThreadFilterTests(unittest.TestCase):
+    def test_thread_filter_same_thread(self):
+        from natcap.invest.utils import ThreadFilter
+
+        # name, level, pathname, lineno, msg, args, exc_info, func=None
+        record = logging.LogRecord(
+            name='foo',
+            level=logging.INFO,
+            pathname=__file__,
+            lineno=500,
+            msg='some logging message',
+            args=(),
+            exc_info=None,
+            func='test_thread_filter_same_thread')
+        filterer = ThreadFilter(threading.currentThread().name)
+
+        # The record comes from the same thread.
+        self.assertEqual(filterer.filter(record), True)
+
+    def test_thread_filter_different_thread(self):
+        from natcap.invest.utils import ThreadFilter
+
+        # name, level, pathname, lineno, msg, args, exc_info, func=None
+        record = logging.LogRecord(
+            name='foo',
+            level=logging.INFO,
+            pathname=__file__,
+            lineno=500,
+            msg='some logging message',
+            args=(),
+            exc_info=None,
+            func='test_thread_filter_same_thread')
+        filterer = ThreadFilter('Thread-nonexistent')
+
+        # The record comes from the same thread.
+        self.assertEqual(filterer.filter(record), False)
