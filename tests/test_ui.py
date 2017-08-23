d39c6566a13c7bcded126497d1775d7080956563
James Douglass <jdouglass@stanford.edu>
2017-08-23 14:15:06 -0700

    Merging changes from develop into this branch.
    Manually resolved a merge conflict on versions.json.
    RE:#3610

---
+++ tests/test_ui.py
@@ -0,0 +1,20 @@
+import unittest
+
+import six
+
+
+class UtilitiesTest(unittest.TestCase):
+    def test_print_args(self):
+        from natcap.invest.cli import _format_args
+
+        args_dict = {
+            'some_arg': [1, 2, 3, 4],
+            'foo': 'bar',
+        }
+
+        args_string = _format_args(args_dict=args_dict)
+        expected_string = six.text_type(
+            'Arguments:\n'
+            'foo      bar\n'
+            'some_arg [1, 2, 3, 4]\n')
+        self.assertEqual(args_string, expected_string)
